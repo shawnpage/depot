4f90d74debfa4c1690eae57e8a2acceebcb91214
Shawn Page <bspage@webassign.net>
2012-04-23 12:40:47 -0400

    WIP - Chapter 9 stuff

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, uniqueness: true
   validates :title, :description, :image_url, presence: true
   validates :title, :length => { :minimum => 5, :too_short => "Title must be at least 5 characters."}
@@ -7,4 +11,16 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  private
+  
+  # ensure that there are not line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return fals
+    end
+  end
 end
