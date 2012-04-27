758b248da523f45bf2be1fc3ce0d2b176dee9976
Shawn Page <bspage@webassign.net>
2012-04-27 12:43:18 -0400

    End of Chapter 9 exercises.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,16 @@
 class StoreController < ApplicationController
   def index
+    @access_count = increment_access_count
     @products = Product.order(:title)
+  end
+end
+
+
+def increment_access_count
+  # Keep a count of how mant times the index action is accessed.
+  if session[:counter].nil?
+    session[:counter] = 1
+  else
+    session[:counter] += 1
   end
 end
